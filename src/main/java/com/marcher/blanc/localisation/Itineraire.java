adc6add0588d886709090c5e1c0ad8739ae7498a
ZoFitahiana <hei.tsiaroniaina@gmail.com>
2024-06-18 09:51:50 +0200

    fix: Iteritaire::update methode

--- src/main/java/com/marcher/blanc/localisation/Itineraire.java
+++ src/main/java/com/marcher/blanc/localisation/Itineraire.java
@@ -1,19 +1,50 @@
 package com.marcher.blanc.localisation;
 
+import com.marcher.blanc.localisation.carte.Carte;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 @Getter
 @Setter
 @AllArgsConstructor
-public class Marche {
-    private List<LieuComposer> listeLieuOrdonnees;
-    public void printMarche() {
-        for (LieuComposer lc : listeLieuOrdonnees) {
-            System.out.println(lc.getLieu1().getName() + " -> " + lc.getLieu2().getName() + " via " + lc.getRue());
+@ToString
+public class Itineraire {
+    private List<Rue> listeRue;
+    public static Itineraire getIteneraire(Carte carte, Lieu depart, Lieu destination) {
+        List<Rue> listeLieuOrdonnees = new ArrayList<>();
+        Lieu currentLieu = depart;
+        Random random = new Random();
+
+        while (!currentLieu.equals(destination)) {
+            List<Rue> possibles = new ArrayList<>();
+            for (Rue lc : carte.getEnsembleLieuInterconnecte()) {
+                if (lc.getLieu1().equals(currentLieu)) {
+                    possibles.add(lc);
+                }
+            }
+            if (possibles.isEmpty()) {
+                throw new RuntimeException("Il n'y a  pas de chemin a trouver pour " + currentLieu.getName() + " pour aller  " + destination.getName());
+            }
+            Rue prochaineLieu = possibles.get(random.nextInt(possibles.size()));
+            listeLieuOrdonnees.add(prochaineLieu);
+            currentLieu = prochaineLieu.getLieu2();
         }
+        return new Itineraire(listeLieuOrdonnees);
+    }
+    public String printMarche() {
+        StringBuilder builder = new StringBuilder();
+        for (Rue lc : listeRue) {
+            builder.append(lc.getLieu1().getName()).append(" -> ");
+            if (lc.equals(listeRue.get(listeRue.size()-1))){
+            builder.append(lc.getLieu2().getName());
+            }
+        }
+        return builder.toString();
     }
 }
